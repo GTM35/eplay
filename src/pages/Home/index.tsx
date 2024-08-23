284223dc5f80c9b05f7399af7156b2c28d44f25f
GTM35 <035guuh@gmail.com>
2024-08-23 11:07:11 -0300

    declaração de types

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,36 +3,6 @@ import ProductList from '../../components/ProductList'
 
 import { useGetOnSaleQuery, useGetSoonQuery } from '../../services/api'
 
-export interface GalleryItem {
-  type: 'imagem' | 'video'
-  url: string
-}
-
-export type Game = {
-  id: number
-  name: string
-  description: string
-  release_date?: string
-  prices: {
-    discount?: number
-    old?: number
-    current?: number
-  }
-  details: {
-    category: string
-    system: string
-    developer: string
-    publisher: string
-    languages: string[]
-  }
-
-  media: {
-    thumbnail: string
-    cover: string
-    gallery: GalleryItem[]
-  }
-}
-
 const Home = () => {
   const { data: onSaleGames, isLoading: isLoadingSales } = useGetOnSaleQuery()
   const { data: soonGame, isLoading: isLoadingSoon } = useGetSoonQuery()
